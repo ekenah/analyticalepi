24a293d49768c9ac3fc16c3ed04de8b203f09270
Eben Kenah <kenah.1@osu.edu>
2025-04-08 01:32:51 -0400

    IPTW

--- R/simpson-std-plot.R
+++ R/simpson-std-plot.R
@@ -26,14 +26,24 @@ p0f <- risk(with(simpson, treated == 0 & male == 0))
 p1m <- risk(with(simpson, treated == 1 & male == 1))
 p0m <- risk(with(simpson, treated == 0 & male == 1))
 
-# standardized risks (standard population half female and half male)
-propf <- 0.5
-propm <- 1 - pf
-p1std <- propf * p1f + propm * p1m
-p0std <- propf * p0f + propm * p0m
+# standardized risks as a function of proportion female
+# Both functions are designed to work if propf is a vector.
+p1std <- function(propf) {
+  propm <- 1 - propf
+  propf * p1f + propm * p1m
+}
+p0std <- function(propf) {
+  propm <- 1 - propf
+  propf * p0f + propm * p0m
+}
 
-# standardized measures of association
-RDstd <- p1std - p0std
-RRstd <- p1std / p0std
-ORstd <- odds(p1std) / odds(p0std)
-CHRstd <- chaz(p1std) / chaz(p0std)
+# plot of standardized risk ratio, odds ratio, and cumulative hazard ratio
+propf <- seq(0, 1, by = 0.01)
+plot(propf, p1std(propf) / p0std(propf), type = "l", ylim = c(0.8, 1),
+     xlab = "Proportion female in the standard population",
+     ylab = "Standardized measure of association")
+lines(propf, odds(p1std(propf)) / odds(p0std(propf)), lty = "dashed")
+lines(propf, chaz(p1std(propf)) / chaz(p0std(propf)), lty = "dotted")
+grid()
+legend("topleft", bg = "white", lty = c("solid", "dotted", "dashed"),
+       legend = c("Risk ratio", "Cumulative hazard ratio", "Odds ratio"))
